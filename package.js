3cd8426527acd341cb2e6d6da418c2dd30083353
Arthur Leonard Andersen <leoc.git@gmail.com>
2015-02-21 01:38:28 +0100

    Set coffeescript dependency version to 1.0.5

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.on_use(function (api) {
   api.use(['service-configuration@1.0.3'], ['server']);
   api.use(['xolvio:http-interceptor@0.3.0'], ['server']);
   api.use(['underscore@1.0.2', 'iron:router@1.0.6'], ['server']);
-  api.use(['coffeescript'], ['client', 'server']);
+  api.use(['coffeescript@1.0.5'], ['client', 'server']);
   api.add_files(['linkedin-stub-client.coffee'], ['client']);
   api.add_files(['linkedin-stub-server.coffee'], ['server']);
   api.export('LinkedinStub', 'server');
